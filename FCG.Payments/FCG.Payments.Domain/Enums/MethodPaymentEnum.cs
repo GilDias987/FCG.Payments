bcd8deb12259a8827cce3f26fe6734fef6b0e14f
gilfi <gilfi@GIL>
2026-01-12 19:49:05 -0300

    Ajustar Api de pagamento

---
+++ FCG.Payments/FCG.Payments.Domain/Enums/MethodPaymentEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace FCG.Payments.Domain.Enums
+{
+    public enum MethodPaymentEnum
+    {
+        [Description("Cartão")]
+        Card,
+        [Description("Boleto")]
+        Ticket,
+        [Description("Pix")]
+        Pix
+    }
+}
---
+++ FCG.Payments/FCG.Payments.Domain/Enums/StatusPaymentEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace FCG.Payments.Domain.Enums
+{
+    public enum StatusPaymentEnum
+    {
+        [Description("Autorizado")]
+        Authorized,
+        [Description("Não Autorizado")]
+        Unauthorized
+    }
+}
