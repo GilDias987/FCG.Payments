5ba67eca26ae3dfcffc12ff17ab2d02e54baffbc
DESKTOP-0SJTD8R\Alexandre Silva <horizonprog@gmail.com>
2026-05-12 00:00:20 -0300

    Redis, MongoDB.

--- FCG.Payments/FCG.Payments.Application.UseCases/Feature/Payment/Consumers/MakePayment/MakePaymentConsumer.cs
+++ FCG.Payments/FCG.Payments.Application.UseCases/Feature/Payment/Consumers/MakePayment/MakePaymentConsumer.cs
@@ -2,10 +2,6 @@
 using FCG.Payments.Domain.Enums;
 using MassTransit;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Text;
 
 namespace FCG.Payments.Application.UseCases.Feature.Payment.Consumers.MakePayment
 {
@@ -18,16 +14,15 @@ namespace FCG.Payments.Application.UseCases.Feature.Payment.Consumers.MakePaymen
         }
         public Task Consume(ConsumeContext<FCG.Shared.Contracts.OrderPlacedEvent> context)
         {
-
-            FCG.Payments.Domain.Enums.MethodPaymentEnum typePayment = (MethodPaymentEnum)char.Parse(context.Message.PaymentMethod);
+            MethodPaymentEnum typePayment = (MethodPaymentEnum)char.Parse(context.Message.PaymentMethod);
 
             return _mediator.Send(new AddPaymentCommand
             {
                 GameId = context.Message.GameId,
                 UserId = context.Message.UserId,
-                Price = context.Message.Price.Value,
+                Price  = context.Message.Price!.Value,
                 MethodPayment = typePayment,
-                StatusPayment = FCG.Payments.Domain.Enums.StatusPaymentEnum.Approved,
+                StatusPayment = StatusPaymentEnum.Approved,
                 Game = context.Message.Game,    
                 Name = context.Message.Name,
                 Email = context.Message.Email
